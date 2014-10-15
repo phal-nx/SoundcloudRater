9edbadaf977a7ea612eac3819b6f336faf7300de
Phil <phdrummer80@gmail.com>
2014-10-14 22:20:42 -0600

    Fixed highlighting

---
+++ bcolors.py
@@ -0,0 +1,21 @@
+class BColors:
+	HEADER = "\033[95m"
+	BLUE = "\033[94m"
+	GREEN = "\033[92m"
+	WARNING = "\033[93m"
+	RED = "\033[91m"
+	ENDC = "\033[0m"
+	BOLD = "\033[1m"
+
+	
+	def makeGreen(self,entryText):
+		return self.GREEN + entryText+ self.ENDC
+
+	def makeBlue(self,entryText):
+		return self.BLUE + entryText+ self.ENDC
+
+	def makeRed(self,entryText):
+		return self.RED + self.BOLD + entryText+ self.ENDC
+	
+	def makeError(self,entryText):
+		return self.WARNING + entryText+ self.ENDC
