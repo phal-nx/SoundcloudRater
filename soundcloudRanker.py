09f9273084114e63e2ddf98041da734de989d59a
Phil <phdrummer80@gmail.com>
2014-10-20 15:43:45 -0600

    Included Generators and made more pep8 compatible

--- soundcloudRanker.py
+++ soundcloudRanker.py
@@ -1,5 +1,5 @@
-import string
 import soundcloud
+# import urllib2
 import re
 import queue
 import threading
@@ -9,131 +9,130 @@ from bcolors import BColors
 from utils import *
 
 BColors = BColors()
-#GLOBAL VARIABLES
-QUERYLENGTH = 1000 #How many twitter posts to search each time this is run
+# GLOBAL VARIABLES
+QUERYLENGTH = 1000  # How many twitter posts to search each time this is run
 QUERY = "soundcloud.com/"
-#Queue to store results
+# Queue to store results
 requestcount = 0
 q = queue.Queue()
-#Setup soundcloud client
-SCClient =  soundcloud.Client(client_id = 'fc2d2bb48658c6612489eed9aaa88dc4')
-
+# Setup soundcloud client
+SCClient = soundcloud.Client(client_id='fc2d2bb48658c6612489eed9aaa88dc4')
+logging.basicConfig(filename='messages.log')
 
 class NotATrack(Exception):
     pass
 
+'''Input: Takes a soundcloud URL
+Returns a track with a bunch of parameters
+(see https://developers.soundcloud.com/docs/api/reference#tracks)
 '''
-Input: Takes a soundcloud URL
-Output: Returns a track with a bunch of parameters (see https://developers.soundcloud.com/docs/api/reference#tracks) 
-'''
+
+
 def getTrackInfo(trackURL):
-	global SCClient
-	try:	
-		track = SCClient.get('/resolve', url=trackURL)
-	except:
-		print(BColors.makeError("Track Not Found: " + trackURL))
-		return False
-	return track 
+        global SCClient
+        try:
+            track = SCClient.get('/resolve', url=trackURL)
+        except:
+            logging.info(BColors.makeError("Track Not Found: " + trackURL))
+            return False
+        return track
+
+'''Input: Track, twitter username, post, soundcloudLink and post_id
+Returns a dictionary to append to the list if it works
+'''
 
 
+def makeEntry(track, username, post, post_id):
+        sctitle = track.title
+        scuser = track.user['username']
+        sctrackduration = track.duration
+        sctracktype = track.track_type
+        soundcloudLink = track.permalink_url
 
+        # To implement at the web level
+        # purchase_url = track.purchase_url
+        # download_url = track.download_url
+        # artwork_url = track.artwork_url
+        # if isinstance(sctracktype,str) and ( sctracktype == "original" or sctracktype == "remix"):
+        return{'username': username, 'post': post, 'id': post_id,
+               'soundcloudLink': soundcloudLink, 'user': scuser,
+               'title': sctitle, 'duration': sctrackduration,
+               'tracktype': sctracktype}
+        #else:
+        #       print("#####"+str(sctracktype)+"###")
+        #       raise Exception("NotATrack")
+'''Input: None
+Creates a and fills it with the Soundcloud Links
+contained in the first QUERYLENGTH amount of
+twitter posts returned by the QUERY if they are valid
+(determined by checkEntry())
 '''
-Input: Takes track, twitter username, post, soundcloudLink and post_id
-Output: Returns a dictionary to append to the list if it works
-'''
-def makeEntry(track,username,post,post_id):
-	sctitle = track.title
-	scuser = track.user['username']
-	sctrackduration = track.duration
-	sctracktype = track.track_type
-	soundcloudLink = track.permalink_url
 
-	##To implement at the web level
-	#purchase_url = track.purchase_url
-	#download_url = track.download_url
-	#artwork_url = track.artwork_url
-	#if isinstance(sctracktype,str) and ( sctracktype == "original" or sctracktype == "remix"):
-	return{'username':username,'post':post,'id':post_id,'soundcloudLink':soundcloudLink,'user':scuser,'title':sctitle,'duration':sctrackduration,'tracktype':sctracktype}
-	#else:
-	#	print("#####"+str(sctracktype)+"###")
-	#	raise Exception("NotATrack")
-'''
-Creates a list and fills it with the Soundcloud Links contained in the first QUERYLENGTH
-amount of twitter posts returned by the QUERY if they are valid (determined by checkEntry())
-'''
+
 def populateList():
-	global QUERYLENGTH
-	global QUERY
-	global q
-	#global requestcount
-	
+        global QUERYLENGTH
+        global QUERY
+        global q
+        #global requestcount
+    
+        # App key and secret found by registering Twitter App
+        APPSECRET = "CPMs6yeXwWRqV5Yow7QmOVZzfouC2UOT1AIykCZKYwBzuUrw0b"
+        APPKEY = 'ngJbJ1mb6uHR0oZvTO5QjPUtz'
+        twitter = Twython(APPKEY, APPSECRET)
+        results = twitter.search(q=QUERY, lang='en',count=QUERYLENGTH)  #since_id =  currentID
+        
+        # Threading is efficient because we're waiting on api requests 
+        threads = [threading.Thread(target=resolveEntry, args=(twitter, results, entry)) 
+                  for entry in results["statuses"]]  # Thread Pool Generator
+        for thread in threads:
+            thread.start()
 
-	#App key and secret found by registering Twitter App
-	APPSECRET = "CPMs6yeXwWRqV5Yow7QmOVZzfouC2UOT1AIykCZKYwBzuUrw0b"
-	APPKEY = 'ngJbJ1mb6uHR0oZvTO5QjPUtz'
-	twitter = Twython(APPKEY, APPSECRET)
-	results = twitter.search(q=QUERY, lang='en',count=QUERYLENGTH) #since_id =  currentID
-	
-	#Threading is much more efficient because we're waiting on 
-	threads = [] #Thread Pool
-	for entry in results["statuses"]:
-		t = threading.Thread(target=resolveEntry, args = (twitter,results,entry))
-		t.start()
-		threads.append(t)
-	for thread in threads:
-		thread.join()
-	songList = q
-	return songList
+        for thread in threads:
+                thread.join()
+        
+        songList = q
+        return songList
 
 
-def resolveEntry(twitter, results,entry):
-	global q
-	for url in entry["entities"]["urls"]:
-		if(checkEntry(url)):
-			postid= entry["id"] 
-			username = entry["user"]["name"]
-			post = sanitizeEntry(entry["text"]) # Remove non ASCII characters
-			soundcloudLink = url["expanded_url"].split('?')[0] #Set the soundcloud link to be everything before a question mark
-			track = getTrackInfo(soundcloudLink)
-			if track != False:
-				try:
-					entry = makeEntry(track,username,post,postid)
-					print(BColors.makeRed(entry['user']) + ":\t" + BColors.makeBlue(entry['title']))
-					q.put(entry)
-				#except NotATrack:
-				#	print(BColors.makeError('ERROR: NOT A SONG ' + soundcloudLink))
-				except:
-					print(BColors.makeError('ERROR: SONG NOT FOUND ' +soundcloudLink))	
+def resolveEntry(twitter, results, entry):
+        global q
+        for url in entry["entities"]["urls"]:
+                if(checkEntry(url)):
+                        postid = entry["id"] 
+                        username = entry["user"]["name"]
+                        post = sanitizeEntry(entry["text"])  # Remove non ASCII characters
+                        soundcloudLink = url["expanded_url"].split('?')[0] # Set the soundcloud link to be everything before a question mark
+                        track = getTrackInfo(soundcloudLink)
+                        if track is not False:
+                                try:
+                                        entry = makeEntry(track, username, post, postid)
+                                        logging.info(BColors.makeRed(entry['user']) + ":\t"
+                                                + BColors.makeBlue(entry['title']))
+                                        q.put(entry)
+                                #except NotATrack:
+                                #       print(BColors.makeError('ERROR: NOT A SONG ' + soundcloudLink))
+                                except:
+                                        logging.warning(BColors.makeError('ERROR: SONG NOT FOUND ' + soundcloudLink))     
 '''
 The main function
 '''
-def main():
-	
-	#global requestcount
-	searchQuery =  populateList()
-	entries = list()
-	idlist = readinIDs()
-	#Print each entry
-	while(not searchQuery.empty()):
-		result = searchQuery.get()
-		if(result['id'] not in idlist):
-			entries.append(result)
-			print(BColors.makeRed(result['username']) + ":")
-			print(result['post'])
-			print(BColors.makeGreen(result['soundcloudLink']) + '\n')
-		#print(BColors.makeError("\n The total count of entries is:"+ str(requestcount)))
-	outputIDsToFile(entries,"ids.txt")
-	
 
 
-
-
-'''
-	#this is list implementation
-	for result in searchQuery:
-		print(BColors.makeRed(result['username']) + ":")
-		print(result['post'])
-		print(BColors.makeGreen(result['soundcloudLink']) + '\n')
-'''
+def main(): 
+        # global requestcount
+        searchQuery = populateList()
+        entries = list()
+        idlist = readinIDs()
+        # Print each entry
+        while(not searchQuery.empty()):
+                result = searchQuery.get()
+                if(result['id'] not in idlist):
+                        entries.append(result)
+                        print(BColors.makeRed(result['username']) + ":")
+                        print(result['post'])
+                        print(BColors.makeGreen(result['soundcloudLink']) + '\n')
+                #print(BColors.makeError("\n The total count of entries is:"+ str(requestcount)))
+        outputIDsToFile(entries, "ids.txt")
+        
 
 main()
--- utils.py
+++ utils.py
@@ -2,31 +2,35 @@
 Input: None
 Output: Return a dictionary of words and their values from the textfile
 ''' 
-def getWordValues():
-	infile = open("wordValues.txt","r")
-	wordValues = {} 
-	for line in infile:
-		splitline = line.split()
-		wordValues[splitline[0]] = splitline[1]
-	infile.close()
-	return wordValues
 
+
+def getWordValues():
+        infile = open("wordValues.txt", "r")
+        wordValues = {} 
+        for line in infile:
+                splitline = line.split()
+                wordValues[splitline[0]] = splitline[1]
+        infile.close()
+        return wordValues
+
+'''Input: A Track
+The rating of a track
 '''
-Input: A Track
-Output: The rating of a track
-'''
+
+
 def trackRating(track):
         tweetText = track['post']
 
-'''
-Input: Takes the expanded Soundcloud URL
-Output: Returns whether or not link is valid
+'''Input: Takes the expanded Soundcloud URL
+Returns whether or not link is valid
 Put logic for the checking of whether the link is valid here
 TODO: Make a regular expression to ensure link is to a track and not an artist
 '''
+
+
 def checkEntry(entry):
         #Simpler Check w/o regex
-        if "//soundcloud.com/" in entry["expanded_url"]:
+        if "//soundcloud.com/" in entry["expanded_url"] and '/sets' not in entry['expanded_url']:
                 return True
         return False
 '''
@@ -36,23 +40,27 @@ def checkEntry(entry):
 '''
 
 
+'''Input: Takes a twitter post string
+Sanitizes the post to only be printable characters removing emojis and other chars 
 '''
-Input: Takes a twitter post string
-Output: Sanitizes the post to only be printable characters removing emojis and other chars 
-'''     
+
+
 def sanitizeEntry(entry):
         return str(entry.encode('ascii',errors='ignore'))[1:]
-def readinIDs():
-	idlist = list()
-	infile = open ("ids.txt",'r')
-	for line in infile:
-		idlist.append(line)
-	infile.close()	
-	return idlist
 
-def outputIDsToFile(entries,filename):
-	infile = open(filename,"a")
-	for entry in entries:
-		infile.write(str(entry['id'])+'\n')
-	infile.close
-		 		
+
+def readinIDs():
+        idlist = list()
+        infile = open("ids.txt",'r')
+        for line in infile:
+                idlist.append(line)
+        infile.close()  
+        return idlist
+
+
+def outputIDsToFile(entries, filename):
+        infile = open(filename,"a")
+        for entry in entries:
+                infile.write(str(entry['id'])+'\n')
+        infile.close
+                                
