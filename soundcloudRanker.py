44b65426320fe31a62030797e9b451947023aa3c
Phil <phdrummer80@gmail.com>
2014-10-22 21:57:51 -0600

    Updated README (and messed up on my git syntax)

---
+++ soundcloudRanker.py
@@ -0,0 +1,157 @@
+import soundcloud
+import queue
+import threading
+import logging
+from twython import Twython
+from bcolors import BColors
+from utils import *
+
+BColors = BColors()
+# GLOBAL VARIABLES
+QUERYLENGTH = 1000  # How many twitter posts to search each time this is run
+QUERY = "soundcloud.com/"
+# Queue to store results
+requestcount = 0
+entryQueue = queue.Queue()
+repeatedEntries = queue.Queue()
+# Setup soundcloud client
+SCClient = soundcloud.Client(client_id='fc2d2bb48658c6612489eed9aaa88dc4')
+logging.basicConfig(filename='messages.log')
+
+class NotATrack(Exception):
+    pass
+
+'''Input: Takes a soundcloud URL
+Returns a track with a bunch of parameters
+(see https://developers.soundcloud.com/docs/api/reference#tracks)
+'''
+
+
+def getTrackInfo(trackURL):
+        try:
+            track = SCClient.get('/resolve', url=trackURL)
+        except:
+            logging.info(BColors.makeError("Track Not Found: %s" % trackURL))
+            return False
+        return track
+
+'''Input: Track, twitter username, post, soundcloudLink and post_id
+Returns a dictionary to append to the list if it works
+TODO: Implement count
+'''
+
+
+def makeEntry(track, username, post, post_id):
+        sctitle = track.title
+        scuser = track.user['username']
+        sctrackduration = track.duration
+        sctracktype = track.track_type
+        soundcloudLink = track.permalink_url
+        sc_id = track.id
+        # To implement at the web level
+        # purchase_url = track.purchase_url
+        # download_url = track.download_url
+        # artwork_url = track.artwork_url
+        # if isinstance(sctracktype,str) and ( sctracktype == "original" or sctracktype == "remix"):
+        return{'username': username, 'post': post, 'id': post_id,
+               'soundcloudLink': soundcloudLink, 'user': scuser,
+               'title': sctitle, 'duration': sctrackduration,
+               'tracktype': sctracktype, 'scid': sc_id, 'count': 1}
+        #else:
+        #       print("#####"+str(sctracktype)+"###")
+        #       raise Exception("NotATrack")
+
+'''Input: None
+Creates a and fills it with the Soundcloud Links
+contained in the first QUERYLENGTH amount of
+twitter posts returned by the QUERY if they are valid
+(determined by checkEntry())
+'''
+
+
+def populateList():
+        # App key and secret found by registering Twitter App
+        APPSECRET = "CPMs6yeXwWRqV5Yow7QmOVZzfouC2UOT1AIykCZKYwBzuUrw0b"
+        APPKEY = 'ngJbJ1mb6uHR0oZvTO5QjPUtz'
+        twitter = Twython(APPKEY, APPSECRET)
+        results = twitter.search(q=QUERY, lang='en', count=QUERYLENGTH)  # since_id =  currentID
+        
+        # Threading is efficient because we're waiting on api requests 
+        threads = [threading.Thread(target=resolveEntry, args=(results, entry)) 
+                  for entry in results["statuses"]]  # Thread Pool Generator
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+                thread.join()
+        
+        songList = entryQueue
+        return songList
+
+'''Input: twitter client, results, entry
+Extracts info and makes an entry in the queue
+from the inputted list of twitter posts
+'''
+def resolveEntry(results, entry):
+        global requestcount
+        for url in entry["entities"]["urls"]:
+            if(checkEntry(url['expanded_url'])):
+                    requestcount += 1
+                    postid = entry["id"]
+                    try:
+                        username = entry["user"]["name"]
+                    except KeyError:
+                        logging.warning('Couldnt store username at %s' % username)
+                    except TypeError:
+                        logging.warning(entry['user'])
+                    try:
+                        post = sanitizeEntry(entry["text"])  # Remove non ASCII characters
+                    except KeyError:
+                        try:
+                            post = sanitizeEntry(entry['post'])
+                        except KeyError:
+                            logging.warning("Couldnt resolve entry at entry['text'] %s" % str( entry))
+                    soundcloudLink = url["expanded_url"].split('?')[0] # Set the soundcloud link to be everything before a question mark
+                    track = getTrackInfo(soundcloudLink)
+                    if track is not False:
+                        if not songExists():  # If the song is unique in the database
+                            try:
+                                entry = makeEntry(track, username, post, postid)
+                                logging.info(BColors.makeRed(entry['user']) + ":\t"
+                                            + BColors.makeBlue(entry['title']))
+                                entryQueue.put(entry)
+                            except AttributeError:
+                                    logging.warning(BColors.makeError('ERROR: SONG NOT FOUND %s' % soundcloudLink))     
+                        else:
+                            repeatedEntries.put(track['user']+'\t'+track['title'])
+
+'''
+The main function
+'''
+
+
+def main(): 
+        searchQuery = populateList()
+        entries = list()
+        idlist = readinIDs()
+        # Print each entry in the queue and populate a list called entries
+        while(not searchQuery.empty()):
+            result = searchQuery.get()
+            if(result['id'] not in idlist):
+                entries.append(result)
+                print(BColors.makeRed(result['username']) + ":")
+                print(result['post'])
+                print(BColors.makeGreen(result['soundcloudLink']) + '\n')
+        allEntries = readInEntries()+entries  # Returns list of all current and past entries (list)
+        while(not repeatedEntries.empty()):  # Aquire all duplicate songs
+            repeatedEntry = repeatedEntries.get()
+            #allEntries['repeatedEntry]['count'] += 1
+            pass
+
+        logging.info(BColors.makeError(" The total count of entries is: %s" % str(requestcount)))
+        outputIDsToFile(entries)  # Makes file to avoid duplicate IDs
+        outputEntriesToFile(entries)  # Outputs entries to use in ranking
+        #wordValues = getWordValues()
+        #(entry['rating'] = rate(entry) for entry in entries)
+        # getTopTen(entriesDict())
+main()
---
+++ utils.py
@@ -0,0 +1,133 @@
+import logging
+import re
+import os
+idfilename = 'ids.txt'
+entriesfilename = 'entries.txt'
+logging.basicConfig(level= logging.INFO, filename='messages.log')
+
+'''
+Input: None
+Output: Return a dictionary of words and their values from the textfile
+''' 
+
+
+def getWordValues():
+        infile = open("wordValues.txt", "r")
+        wordValues = {} 
+        for line in infile:
+                splitline = line.split()
+                wordValues[splitline[0]] = splitline[1]
+        infile.close()
+        return wordValues
+
+
+'''Input: AllEntries
+Return list with top 10 songs
+TODO
+'''
+def getTopTen(allEntries):
+    for entry in allEntries:
+        pass
+
+'''Input: A Track
+The rating of a track
+TODO: Implement All
+'''
+
+
+def rate(track):
+        tweetText = track['post']
+        return 5
+
+'''Input: Takes the expanded Soundcloud URL
+Returns whether or not link is valid
+Put logic for the checking of whether the link is valid here
+'''
+
+regex = re.compile("((http|https)://soundcloud.com\/\S+\/\S+)")
+def checkEntry(entry):
+    if type(entry) is str:
+        return bool(regex.match(entry)) and 'sets' not in entry
+    return False
+
+
+'''Input: Takes a twitter post string
+Sanitizes the post to only be printable characters removing emojis and other chars 
+'''
+
+
+def sanitizeEntry(entry):
+        return str(entry.encode('ascii',errors='ignore'))[1:].replace('/m.s','/s') #Accounts for mobile links
+
+'''Input: None
+Returns a list with all ids in ids.txt
+'''
+def readinIDs():
+        idlist = list()
+        infile = open(idfilename,'r')
+        for line in infile:
+                idlist.append(line)
+        infile.close()  
+        return idlist
+
+'''Input: entries and the filename to output to
+Outputs all the IDs to a file to avoid duplicate results
+'''
+
+
+def outputIDsToFile(entries):
+        infile = open(idfilename,"a")
+        for entry in entries:
+                infile.write(str(entry['id'])+'\n')
+        infile.close
+                                
+
+'''Input: entries and the filename to output to
+Outputs all the IDs to a file to avoid duplicate results
+'''
+
+
+def outputEntriesToFile(entries):
+        infile =  open(entriesfilename,"r+") 
+        oldEntries= readInEntries()
+        if os.stat(entriesfilename)[6] != 0: #  If content in file
+            try:
+                if oldEntries:
+                    infile.seek(0)  # Seek to the beginning in order to overwrite not append
+                    infile.write(str(oldEntries + entries))  # Append if something exists
+                    logging.info("Outputted entries to  %s succesfully" % entriesfilename)
+                else:
+                    logging.info("No old files")
+                    infile.seek(0)
+                    infile.write(str(entries))  # If empty then create
+                infile.close()
+                    
+            except:
+                logging.warning("Unable to output entries to file %s " % entriesfilename)
+        
+'''Input:None
+Returns dictionary externally stored
+'''
+
+
+def readInEntries():
+    with open(entriesfilename,'r') as infile:
+        if os.stat(entriesfilename)[6] != 0: #  If content in file
+            try:
+                currentLine = eval(infile.read())
+                return currentLine
+            except TypeError:
+                logging.warning('Cant parse entry at %s' % infile.read())
+        return []
+
+'''Input: Track
+Checks to see if song already exists in database
+TODO: Add logic to find if song already exists
+whether by identical URL or similar Artist / Track
+'''
+
+
+def songExists():
+    return False
+    #return (song in database)
+        
